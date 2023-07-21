3a390d98094e301edbe24b3f2f77d338e8282ea7
philica <fikretucker79@gmail.com>
2023-07-21 12:41:38 +0300

    Initializing scene bot

--- bot.js
+++ bot.js
@@ -1,29 +1,46 @@
-const { Telegraf } = require('telegraf')
+const Telegraf = require('telegraf')
+const Composer = require('telegraf/composer')
 const session = require('telegraf/session')
 const Stage = require('telegraf/stage')
-const Scene = require('telegraf/scenes/base')
-const { leave } = Stage
+const Markup = require('telegraf/markup')
+const WizardScene = require('telegraf/scenes/wizard')
 
-const bot = new Telegraf('6229454880:AAELolbagiO2DPkEquD5lT2vLUGlJFT_wAs');
+const stepHandler = new Composer()
+stepHandler.action('next', (ctx) => {
+  ctx.reply('Step 2. Via inline button')
+  return ctx.wizard.next()
+})
+stepHandler.command('next', (ctx) => {
+  ctx.reply('Step 2. Via command')
+  return ctx.wizard.next()
+})
+stepHandler.use((ctx) => ctx.replyWithMarkdown('Press `Next` button or type /next'))
 
+const superWizard = new WizardScene('super-wizard',
+  (ctx) => {
+    ctx.reply('Step 1', Markup.inlineKeyboard([
+      Markup.urlButton('❤️', 'http://telegraf.js.org'),
+      Markup.callbackButton('➡️ Next', 'next')
+    ]).extra())
+    return ctx.wizard.next()
+  },
+  stepHandler,
+  (ctx) => {
+    ctx.reply('Step 3')
+    return ctx.wizard.next()
+  },
+  (ctx) => {
+    ctx.reply('Step 4')
+    return ctx.wizard.next()
+  },
+  (ctx) => {
+    ctx.reply('Done')
+    return ctx.scene.leave()
+  }
+)
 
-
-
-// Greeter scene
-const greeter = new Scene('greeter')
-greeter.enter((ctx) => ctx.reply('Hi'))
-
-greeter.leave((ctx) => ctx.reply('Bye'))
-greeter.hears(/hi/gi, leave())
-greeter.on('message', (ctx) => ctx.reply('Send `hi`'))
-
-// Create scene manager
-const stage = new Stage()
-stage.command('cancel', leave())
-
-// Scene registration
-stage.register(greeter)
+const bot = new Telegraf(process.env.BOT_TOKEN)
+const stage = new Stage([superWizard], { default: 'super-wizard' })
 bot.use(session())
 bot.use(stage.middleware())
-bot.command('greeter', (ctx) => ctx.scene.enter('greeter'))
-bot.startPolling()
+bot.launch()
