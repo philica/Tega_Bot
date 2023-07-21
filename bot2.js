12c5eeb5c17b76876df216e65fabfd9192c3117d
philica <fikretucker79@gmail.com>
2023-07-21 23:17:37 +0300

    Implement ride Quoting function

--- bot2.js
+++ bot2.js
@@ -1,5 +1,10 @@
-const { Telegraf, Scenes } = require('telegraf');
-const { WizardScene, Stage } = Scenes;
+const Telegraf = require('telegraf')
+const Composer = require('telegraf/composer')
+const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Markup = require('telegraf/markup')
+const WizardScene = require('telegraf/scenes/wizard')
+const { enter, leave } = Stage
 
 const bot = new Telegraf('6229454880:AAELolbagiO2DPkEquD5lT2vLUGlJFT_wAs');
 
@@ -10,34 +15,59 @@ console.log("bot starting ... ");
 const registrationWizard = new WizardScene(
   'nameScene',
   (ctx) => {
-    ctx.reply('Welcome, enter your name');
+    ctx.reply('Please enter pickup location');
     ctx.wizard.state.user = {};
     return ctx.wizard.next();
   },
   (ctx) => {
-    ctx.wizard.state.user.name = ctx.message.text;
-    ctx.reply('Enter your age');
+    ctx.wizard.state.user.pickupLocation = ctx.message.text;
+    ctx.reply('Please enter Destination location');
     return ctx.wizard.next();
   },
   (ctx) => {
-    ctx.wizard.state.user.age = ctx.message.text;
+    ctx.wizard.state.user.destinationLocation = ctx.message.text;
+    ctx.reply('Please enter Pick up time');
+    return ctx.wizard.next();
+  },
+
+  (ctx) => {
+    ctx.wizard.state.user.pickupTime = parseInt(ctx.message.text);
+    ctx.reply('Please enter prefered gender');
     return ctx.wizard.next();
   },
   (ctx) => {
+    ctx.wizard.state.user.preferedGender = ctx.message.text;
+    ctx.reply('Please enter Note');
+    return ctx.wizard.next();
+  }
+  ,
+  (ctx) => {
+    ctx.wizard.state.user.note = ctx.message.text;
+    let userData = ctx.wizard.state.user
+    console.log(userData)
     ctx.reply(
       `Thanks for filling out the information
-       your name = ${ctx.wizard.state.user.name}
-       your age = ${ctx.wizard.state.user.age}
+       Pickup location = ${ctx.wizard.state.user.pickupLocation}
+       Destination location  = ${ctx.wizard.state.user.destinationLocation}
+       Pickup time  = ${ctx.wizard.state.user.pickupTime}
+       Prefered gender  = ${ctx.wizard.state.user.preferedGender}
+       Note  = ${ctx.wizard.state.user.note}
+
       `
     );
     return ctx.scene.leave();
   }
 );
 
-const stage = new Stage([registrationWizard]);
+// const stage = new Stage([registrationWizard], { default: 'nameScene' });
 
+const stage = new Stage()
+stage.register(registrationWizard)
+
+bot.use(session())
 bot.use(stage.middleware());
-bot.hears('hi',(ctx) => {
-    ctx.scene.enter('nameScene')
-});
+// bot.hears('hi',(ctx) => {
+//     return ctx.scene.enter('nameScene')
+// });
+bot.start((ctx) => ctx.scene.enter('nameScene'))
 bot.launch();
